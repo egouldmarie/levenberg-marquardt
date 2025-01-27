583a37f39e4e44dbb4816ccc21101b9d006a7456
egouldmarie <e.gould.marie@gmail.com>
2025-01-27 14:46:06 -0700

    levenbergMarquardt fitting requests animation frame

--- src/index.js
+++ src/index.js
@@ -19,98 +19,123 @@ values of λ result in a gradient descent update
  * @param {ArrayLike<number>} [options.maxValues] - Maximum allowed values for parameters
  * @param {number} [options.maxIterations = 100] - Maximum of allowed iterations
  * @param {number} [options.errorTolerance = 10e-3] - Minimum uncertainty allowed for each point.
- * @param {number} [options.timeout] - maximum time running before throw in seconds.
  * @return {{parameterValues: Array<number>, parameterError: number, iterations: number}}
  */
-export function levenbergMarquardt(data, parameterizedFunction, options) {
-  let {
-    checkTimeout,
-    minValues,
-    maxValues,
-    parameters,
-    weightSquare,
-    damping,
-    dampingStepUp,
-    dampingStepDown,
-    maxIterations,
-    errorTolerance,
-    errorCalculation,
-    centralDifference,
-    gradientDifference,
-    improvementThreshold,
-  } = checkOptions(data, parameterizedFunction, options);
+export class levenbergMarquardt {
+  constructor(data, parameterizedFunction, options) {
+    let opts = checkOptions(data, parameterizedFunction, options);
+    this.data = data;
+    this.parameterizedFunction = parameterizedFunction;
 
-  let error = errorCalculation(
-    data,
-    parameters,
-    parameterizedFunction,
-    weightSquare,
-  );
-  let optimalError = error;
-  let optimalParameters = parameters.slice();
+    this.minValues = opts.minValues;
+    this.maxValues = opts.maxValues;
+    this.parameters = opts.parameters;
+    this.weightSquare = opts.weightSquare;
+    this.damping = opts.damping;
+    this.dampingStepUp = opts.dampingStepUp;
+    this.dampingStepDown = opts.dampingStepDown;
+    this.maxIterations = opts.maxIterations;
+    this.errorTolerance = opts.errorTolerance;
+    this.errorCalculation = opts.errorCalculation;
+    this.centralDifference = opts.centralDifference;
+    this.gradientDifference = opts.gradientDifference;
+    this.improvementThreshold = opts.improvementThreshold;
 
-  let converged = error <= errorTolerance;
-
-  let iteration = 0;
-  for (; iteration < maxIterations && !converged; iteration++) {
-    let previousError = error;
-
-    let { perturbations, jacobianWeightResidualError } = step(
-      data,
-      parameters,
-      damping,
-      gradientDifference,
-      parameterizedFunction,
-      centralDifference,
-      weightSquare,
-    );
-
-    for (let k = 0; k < parameters.length; k++) {
-      parameters[k] = Math.min(
-        Math.max(minValues[k], parameters[k] - perturbations.get(k, 0)),
-        maxValues[k],
-      );
-    }
-
-    error = errorCalculation(
-      data,
-      parameters,
-      parameterizedFunction,
-      weightSquare,
-    );
-
-    if (isNaN(error)) break;
-
-    if (error < optimalError - errorTolerance) {
-      optimalError = error;
-      optimalParameters = parameters.slice();
-    }
-
-    let improvementMetric =
-      (previousError - error) /
-      perturbations
-        .transpose()
-        .mmul(perturbations.mul(damping).add(jacobianWeightResidualError))
-        .get(0, 0);
-
-    if (improvementMetric > improvementThreshold) {
-      damping = Math.max(damping / dampingStepDown, 1e-7);
-    } else {
-      damping = Math.min(damping * dampingStepUp, 1e7);
-    }
-
-    if (checkTimeout()) {
-      throw new Error(
-        `The execution time is over to ${options.timeout} seconds`,
-      );
-    }
-
-    converged = error <= errorTolerance;
+    this.iteration = 0;
+    this._frame = undefined;
   }
 
-  return {
-    parameterValues: optimalParameters,
-    parameterError: optimalError,
-    iterations: iteration,
-  };
+  start() {
+    if (!this._fitting) {
+      this._fitting = true;
+
+      this.iteration = 0;
+      this.error = this.errorCalculation(
+        this.data,
+        this.parameters,
+        this.parameterizedFunction,
+        this.weightSquare,
+      );
+      this.optimalError = this.error;
+      this.optimalParameters = this.parameters.slice();
+      this.fit();
+    }
+  }
+
+  stop() {
+    this._fitting = false;
+    if (this._frame) window.cancelAnimationFrame(this._frame);
+  }
+
+  getResults() {
+    return {
+      parameterValues: this.optimalParameters,
+      parameterError: this.optimalError,
+      iterations: this.iteration,
+    };
+  }
+
+  fit() {
+    if (this._fitting) {
+      this._frame = window.requestAnimationFrame(this.fit);
+
+      let converged = this.error <= this.errorTolerance;
+
+      if (!converged && this.iteration < this.maxIterations) {
+        this.iteration += 1;
+        let previousError = this.error;
+
+        let { perturbations, jacobianWeightResidualError } = step(
+          this.data,
+          this.parameters,
+          this.damping,
+          this.gradientDifference,
+          this.parameterizedFunction,
+          this.centralDifference,
+          this.weightSquare,
+        );
+
+        for (let k = 0; k < this.parameters.length; k++) {
+          this.parameters[k] = Math.min(
+            Math.max(
+              this.minValues[k],
+              this.parameters[k] - perturbations.get(k, 0),
+            ),
+            this.maxValues[k],
+          );
+        }
+
+        this.error = this.errorCalculation(
+          this.data,
+          this.parameters,
+          this.parameterizedFunction,
+          this.weightSquare,
+        );
+
+        if (isNaN(this.error)) {
+          throw new Error('Error should be real value: ' + this.error);
+        }
+
+        if (this.error < this.optimalError - this.errorTolerance) {
+          this.optimalError = this.error;
+          this.optimalParameters = this.parameters.slice();
+        }
+
+        let improvementMetric =
+          (previousError - this.error) /
+          perturbations
+            .transpose()
+            .mmul(
+              perturbations.mul(this.damping).add(jacobianWeightResidualError),
+            )
+            .get(0, 0);
+
+        if (improvementMetric > this.improvementThreshold) {
+          this.damping = Math.max(this.damping / this.dampingStepDown, 1e-7);
+        } else {
+          this.damping = Math.min(this.damping * this.dampingStepUp, 1e7);
+        }
+      }
+    }
+  }
 }
